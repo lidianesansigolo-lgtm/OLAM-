6d1ad23cec7414b35735461b586c3118e5c671aa
lidianesansigolo-lgtm <lidianesansigolo@hotmail.com>
2026-05-13 13:25:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   plugins: [
     // Configuramos o plugin para encontrar suas rotas e gerar o arquivo na raiz
     TanStackRouterVite({
+      tailwindcss(),//
       routesDirectory: './src/routes',      // Verifique se suas rotas estão em src/routes
       generatedRouteTree: './routeTree.gen.ts', 
       quoteStyle: 'single',
@@ -17,7 +18,6 @@ export default defineConfig({
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
-    tailwindcss(),
     netlify(),
     tanstackStart(),
     viteReact(),
